43db95dab8d6dd36f5f3fa51a1c4045b2a4c2783
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2017-05-11 12:41:42 +0300

    Remove get_h2h_* wrappers.

--- R/head-to-head-functions.R
+++ R/head-to-head-functions.R
@@ -2,13 +2,6 @@
 #'
 #' Specific functions for computing Head-to-Head matrices.
 #'
-#' @param cr_data Competition results in format ready for
-#'   \code{\link[=results-longcr]{to_longcr}}.
-#' @param players Vector of players for which Head-to-Head is computed.
-#' @param absent_players Function which performs actions on Head-to-Head matrix
-#'   dealing with players which absent in \code{cr_data}.
-#' @param absent_h2h Function which performs actions on Head-to-Head matrix
-#'   dealing with absent Head-to-Head records for some pairs of players.
 #' @param matchup_data Data of matchups described in \link{head-to-head} as
 #'   input for specific \code{h2h_fun}.
 #' @param half_for_draw Whether to count half points for draws in
@@ -29,13 +22,8 @@
 #'     \bold{Note} that it excludes matchups of the players with themselves.}
 #' }
 #'
-#' All \code{get_h2h_*} are wrappers around \code{\link[=head-to-head]{get_h2h}}
-#' for using a specific \code{h2h_fun}.
-#'
-#' @return \code{get_h2h_*} return \link[=head-to-head]{Head-to-Head matrices}.
-#'
-#' All \code{h2h_*} are implementations of \code{h2h_fun} and return a single
-#' Head-to-Head value.
+#' @return All \code{h2h_*} are implementations of \code{h2h_fun} and return a
+#'   single Head-to-Head value.
 #'
 #' @examples # Initializing data
 #' cr_data <- data.frame(
@@ -46,9 +34,9 @@
 #' matchup_data <- get_matchups(cr_data)
 #'
 #' # Computing Head-to-Head matrices
-#' get_h2h_num_wins(cr_data)
-#' get_h2h_num_wins(cr_data, half_for_draw = TRUE)
-#' get_h2h_num_wins(cr_data, half_for_draw = TRUE, fill = 0)
+#' get_h2h(cr_data, h2h_num_wins)
+#' get_h2h(cr_data, h2h_num_wins, half_for_draw = TRUE)
+#' get_h2h(cr_data, h2h_num_wins, half_for_draw = TRUE, fill = 0)
 #'
 #' # Computing Head-to-Head values based on matchups
 #' h2h_mean_score_diff(matchup_data[2, ])
@@ -59,41 +47,11 @@
 #' @name head-to-head-functions
 NULL
 
-
-# h2h_mean_score_diff ---------------------------------------------------------
-#' @rdname head-to-head-functions
-#' @export
-get_h2h_mean_score_diff <-
-  function(cr_data, players = NULL, absent_players = players_drop,
-           absent_h2h = fill_h2h, ...) {
-    get_h2h(
-      cr_data = cr_data, h2h_fun = h2h_mean_score_diff,
-      players = players,
-      absent_players = absent_players, absent_h2h = absent_h2h,
-      ...
-    )
-  }
-
 #' @rdname head-to-head-functions
 #' @export
 h2h_mean_score_diff <- function(matchup_data, ...) {
   mean(matchup_data$score2 - matchup_data$score1)
 }
-
-
-# h2h_mean_score --------------------------------------------------------------
-#' @rdname head-to-head-functions
-#' @export
-get_h2h_mean_score <-
-  function(cr_data, players = NULL, absent_players = players_drop,
-           absent_h2h = fill_h2h, ...) {
-    get_h2h(
-      cr_data = cr_data, h2h_fun = h2h_mean_score,
-      players = players,
-      absent_players = absent_players, absent_h2h = absent_h2h,
-      ...
-    )
-  }
 
 #' @rdname head-to-head-functions
 #' @export
@@ -101,60 +59,17 @@ h2h_mean_score <- function(matchup_data, ...) {
   mean(matchup_data$score2)
 }
 
-# h2h_sum_score_diff ----------------------------------------------------------
-#' @rdname head-to-head-functions
-#' @export
-get_h2h_sum_score_diff <-
-  function(cr_data, players = NULL, absent_players = players_drop,
-           absent_h2h = fill_h2h, ...) {
-    get_h2h(
-      cr_data = cr_data, h2h_fun = h2h_sum_score_diff,
-      players = players,
-      absent_players = absent_players, absent_h2h = absent_h2h,
-      ...
-    )
-  }
-
 #' @rdname head-to-head-functions
 #' @export
 h2h_sum_score_diff <- function(matchup_data, ...) {
   sum(matchup_data$score2 - matchup_data$score1)
 }
 
-
-
-# h2h_sum_score ---------------------------------------------------------------
-#' @rdname head-to-head-functions
-#' @export
-get_h2h_sum_score <-
-  function(cr_data, players = NULL, absent_players = players_drop,
-           absent_h2h = fill_h2h, ...) {
-    get_h2h(
-      cr_data = cr_data, h2h_fun = h2h_sum_score,
-      players = players,
-      absent_players = absent_players, absent_h2h = absent_h2h,
-      ...
-    )
-  }
-
 #' @rdname head-to-head-functions
 #' @export
 h2h_sum_score <- function(matchup_data, ...) {
   sum(matchup_data$score2)
 }
-# h2h_num_wins ----------------------------------------------------------------
-#' @rdname head-to-head-functions
-#' @export
-get_h2h_num_wins <-
-  function(cr_data, players = NULL, absent_players = players_drop,
-           absent_h2h = fill_h2h, ...) {
-    get_h2h(
-      cr_data = cr_data, h2h_fun = h2h_num_wins,
-      players = players,
-      absent_players = absent_players, absent_h2h = absent_h2h,
-      ...
-    )
-  }
 
 #' @rdname head-to-head-functions
 #' @export
@@ -164,4 +79,3 @@ h2h_num_wins <- function(matchup_data, half_for_draw = FALSE, ...) {
   score2 <- matchup_data$score2[not_identity]
   sum(score2 > score1) + half_for_draw * 0.5 * sum(score2 == score1)
 }
-
--- tests/testthat/test-head-to-head-functions.R
+++ tests/testthat/test-head-to-head-functions.R
@@ -11,23 +11,6 @@ cr_data <- data.frame(
 matchup_data <- get_matchups(cr_data)
 
 
-
-# get_h2h_mean_score_diff -------------------------------------------------
-test_that("get_h2h_mean_score_diff works", {
-  output <- matrix(c(0, -1, 1, 0), nrow = 2,
-                   dimnames = list(1:2, 1:2))
-  class(output) <- c("h2h", "matrix")
-
-  expect_identical(get_h2h_mean_score_diff(cr_data), output)
-
-  cr_data_1 <- bind_rows(cr_data, cr_data)
-  cr_data_1$game[5:8] <- rep(3:4, each = 2)
-
-  expect_equal(get_h2h_mean_score_diff(cr_data_1), output)
-})
-
-
-
 # h2h_mean_score_diff -----------------------------------------------------
 test_that("h2h_mean_score_diff works", {
   expect_equal(h2h_mean_score_diff(matchup_data), 0)
@@ -39,36 +22,12 @@ test_that("h2h_mean_score_diff works", {
 })
 
 
-
-# get_h2h_mean_score ------------------------------------------------------
-test_that("get_h2h_mean_score works", {
-  output <- matrix(c(32, 32, 33, 33), nrow = 2,
-                   dimnames = list(1:2, 1:2))
-  class(output) <- c("h2h", "matrix")
-
-  expect_identical(get_h2h_mean_score(cr_data), output)
-})
-
 # h2h_mean_score ----------------------------------------------------------
 test_that("h2h_mean_score works", {
   expect_equal(h2h_mean_score(matchup_data[c(2, 6), ]), 33)
   expect_equal(h2h_mean_score(matchup_data[c(1, 5), ]), 32)
 
   expect_silent(h2h_mean_score(matchup_data[c(1, 5), ], extraArg = TRUE))
-})
-
-# get_h2h_sum_score_diff --------------------------------------------------
-test_that("get_h2h_sum_score_diff works", {
-  output <- matrix(c(0, -2, 2, 0), nrow = 2,
-                   dimnames = list(1:2, 1:2))
-  class(output) <- c("h2h", "matrix")
-
-  expect_identical(get_h2h_sum_score_diff(cr_data), output)
-
-  cr_data_1 <- bind_rows(cr_data, cr_data)
-  cr_data_1$game[5:8] <- rep(3:4, each = 2)
-
-  expect_equal(get_h2h_sum_score_diff(cr_data_1), 2*output)
 })
 
 
@@ -83,20 +42,6 @@ test_that("h2h_sum_score_diff works", {
 })
 
 
-# get_h2h_sum_score -------------------------------------------------------
-test_that("get_h2h_sum_score works", {
-  output <- matrix(c(64, 64, 66, 66), nrow = 2,
-                   dimnames = list(1:2, 1:2))
-  class(output) <- c("h2h", "matrix")
-
-  expect_identical(get_h2h_sum_score(cr_data), output)
-
-  cr_data_1 <- bind_rows(cr_data, cr_data)
-  cr_data_1$game[5:8] <- rep(3:4, each = 2)
-
-  expect_equal(get_h2h_sum_score(cr_data_1), 2*output)
-})
-
 # h2h_sum_score -----------------------------------------------------------
 test_that("h2h_sum_score works", {
   expect_equal(h2h_sum_score(matchup_data), sum(matchup_data$score2))
@@ -105,27 +50,6 @@ test_that("h2h_sum_score works", {
   expect_equal(h2h_sum_score(matchup_data_1), 66)
 
   expect_silent(h2h_sum_score(matchup_data_1, extraArg = TRUE))
-})
-# get_h2h_num_wins --------------------------------------------------------
-test_that("get_h2h_num_wins works", {
-  output <- matrix(c(0, 0, 2, 0), nrow = 2,
-                   dimnames = list(1:2, 1:2))
-  class(output) <- c("h2h", "matrix")
-
-  expect_identical(get_h2h_num_wins(cr_data), output)
-
-  cr_data_1 <- bind_rows(cr_data, cr_data)
-  cr_data_1$game[5:8] <- rep(3:4, each = 2)
-
-  expect_equal(get_h2h_num_wins(cr_data_1), 2*output)
-
-  cr_data_1$score[7] <- 34
-  output_1 <- output
-  output_1[1, 2] <- 3
-  expect_equal(get_h2h_num_wins(cr_data_1, half_for_draw = FALSE), output_1)
-  output_1[1, 2] <- 3.5
-  output_1[2, 1] <- 0.5
-  expect_equal(get_h2h_num_wins(cr_data_1, half_for_draw = TRUE), output_1)
 })
 
 
