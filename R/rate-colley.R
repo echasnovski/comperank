a5d3726136bd50ba77376b472383329c1d468a4d
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2017-05-12 19:09:51 +0300

    Add function rate_colley for Colley rating method.

---
+++ R/rate-colley.R
@@ -0,0 +1,56 @@
+#' Colley rating
+#'
+#' Function to compute rating using Colley method.
+#'
+#' @param cr_data Competition results in format ready for
+#'   \code{\link[=results-longcr]{to_longcr}}.
+#' @param players Vector of players for which rating is computed.
+#'
+#' @details This rating method was initially designed for games between two
+#'   players. That is why competition results are transformed into pairwise
+#'   games \code{cr_pair} with \link{to_pairgames}. Data from \code{cr_pair} is
+#'   used in rating computation.
+#'
+#'   Computation is done based only on the matchups between players in
+#'   argument \code{players}. If \code{NULL} then all players present in
+#'   \code{cr_data} are used. \bold{Note} that it isn't necassary for all
+#'   entries in \code{players} to be present in \code{cr_pair} but it might be
+#'   a good idea in order to obtain plausible outcome rating.
+#'
+#'   The outline of the Colley method is as follows:
+#'   \enumerate{
+#'     \item Compute Colley matrix: diagonal elements are equal to number of
+#'       games played by certain player \emph{plus 2}, off-diagonal are equal to
+#'       minus number of common games played. This matrix will be the matrix of
+#'       system of linear equations (SLE);
+#'     \item Compute right-hand side of SLE as 1 + 0.5*("number of player's
+#'       wins" - "number of player's losses");
+#'     \item Solve the SLE. The solution is the Colley rating.
+#'   }
+#'
+#' @return Named vector of the Colley rating. The sum of all ratings should be
+#'   equal to 0.5*"number of players in the rating".
+#'
+#' @examples rate_colley(ncaa2005)
+#' rate_colley(ncaa2005, players = c("UNC", "Duke", "Miami", "UVA", "VT"))
+#' rate_colley(ncaa2005, players = c("UNC", "Miami", "UVA", "VT"))
+#'
+#' @export
+rate_colley <- function(cr_data, players = NULL) {
+  cr <- to_longcr(cr_data, repair = TRUE) %>%
+    to_pairgames()
+
+  # Compute Colley ratings
+  colley_mat <- - get_h2h(cr, h2h_num, players = players,
+                          absent_players = skip_action,
+                          absent_h2h = fill_h2h, fill = 0)
+  diag(colley_mat) <- 0
+  diag(colley_mat) <- 2 - rowSums(colley_mat)
+
+  win_mat <- get_h2h(cr, h2h_num_wins, players = players,
+                     absent_players = skip_action,
+                     absent_h2h = fill_h2h, fill = 0)
+  right_hand <- 1 + 0.5*(colSums(win_mat) - rowSums(win_mat))
+
+  solve(colley_mat, right_hand)
+}
---
+++ tests/testthat/test-rate-colley.R
@@ -0,0 +1,49 @@
+context("rate-colley")
+
+
+# Input and output data ---------------------------------------------------
+input <- ncaa2005
+output <- c(0.21, 0.79, 0.5, 0.36, 0.64)
+names(output) <- c("Duke", "Miami", "UNC", "UVA", "VT")
+
+
+# rate_colley -------------------------------------------------------------
+test_that("rate_colley works with NULL players", {
+  expect_equal(round(rate_colley(input), 2), output)
+  expect_equal(round(rate_colley(input, players = NULL), 2), output)
+})
+
+test_that("rate_colley works with not NULL players", {
+  players <- names(output)[c(5, 2, 1, 3, 4)]
+  expect_equal(round(rate_colley(input, players = players), 2),
+               output[players])
+
+  output_1 <- c(0.3, 0.7, 0.5)
+  names(output_1) <- names(output)[1:3]
+  expect_equal(rate_colley(input, players = names(output)[1:3]),
+               output_1)
+})
+
+test_that("rate_colley works with not all matchups present", {
+  input_1 <- input[1:10, ]
+  output_1 <- c(0.214285714285714, 0.671428571428571, 0.471428571428571,
+                0.571428571428571, 0.571428571428571)
+  names(output_1) <- names(output)
+
+  expect_equal(rate_colley(input_1, players = NULL), output_1)
+})
+
+test_that("rate_colley correctly works with not pair games", {
+  input_nonpair <- data.frame(
+    game = c(rep(1:5, each = 3), 6),
+    player = c(rep(1:5, times = 3), 1),
+    score = c(110L, 111L, 106L, 113L, 108L, 115L, 102L, 105L, 103L, 116L,
+              101L, 114L, 104L, 107L, 109L, 112L),
+    extraCol = -(1:16)
+  )
+  output <- c(0.51123595505618, 0.48876404494382, 0.387640449438202,
+              0.5, 0.612359550561798)
+  names(output) <- 1:5
+
+  expect_equal(rate_colley(input_nonpair), output)
+})
