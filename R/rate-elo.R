cc066fc11b5023cb28197e11d8debad36287c770
evgeni.chasnovski <evgeni.chasnovski@gmail.com>
2017-06-05 20:18:48 +0300

    Implement Elo ratings.
    Functions added: rate_elo, add_elo_ratings, elo.

---
+++ R/rate-elo.R
@@ -0,0 +1,127 @@
+#' Elo ratings
+#'
+#' Functions to compute Elo ratings.
+#'
+#' @param cr_data Competition results of \link{pairgames} in format ready for
+#'   \code{\link[=results-longcr]{to_longcr}}.
+#' @param K K-factor for Elo formula.
+#' @param ksi Normalization coefficient for Elo formula.
+#' @param players Vector of players for which rating is computed.
+#' @param initial_ratings Initial ratings (see
+#'   \link[=rate-iterative]{Iterative ratings}).
+#' @param rating1 Rating of player1 before the game.
+#' @param score1 Score of player1 in the game.
+#' @param rating2 Rating of player2 before the game.
+#' @param score2 Score of player2 in the game.
+#'
+#' @details \code{rate_elo} and \code{add_elo_ratings} are wrappers around
+#'   \code{\link{rate_iterative}} and \code{\link{add_iterative_ratings}}
+#'   correspondingly. Rate function is based on Elo algorithm of updating
+#'   ratings:
+#'   \itemize{
+#'     \item Probability of player1 (with rating r1) winning against player2
+#'       (with rating r2) is computed based on rating difference and sigmoid
+#'       function: \code{P = 1 / (1 + 10^( (r2 - r1) / ksi ) )}. \code{ksi}
+#'       defines the spread of ratings;
+#'     \item Result of the game from player1 perspective is computed based on
+#'       rule: \code{S = 1} (if \code{score1} > \code{score2}), \code{S = 0.5}
+#'       (if \code{score1} == \code{score2}) and \code{S = 0} (if \code{score1}
+#'       < \code{score2});
+#'     \item Rating delta is computed: \code{d = K * (S - P)}. The more the
+#'       \code{K} the more the delta (with other being equal);
+#'     \item New ratings are computed: \code{r1_new = r1 + d},
+#'       \code{r2_new = r2 - d}.
+#'   }
+#'
+#' \code{elo} function implements this algorithm. It is vectorized over all its
+#'   arguments with standard R recycling functionality. \bold{Note} that not
+#'   this function is used in \code{rate_elo} and \code{add_elo_ratings} because
+#'   of its not appropriate output format, but rather its non-vectorized
+#'   reimplementation is.
+#'
+#' @return \code{rate_elo} returns a named vector of Elo ratings by the end of
+#'   competition results.
+#'
+#'   \code{add_elo_ratings} returns a
+#'   \code{\link[=results-widecr]{widecr}} form of \code{cr_data} with four
+#'   rating columns added:
+#'   \itemize{
+#'     \item rating1Before - Rating of player1 before the game;
+#'     \item rating2Before - Rating of player2 before the game;
+#'     \item rating1After - Rating of player1 after the game;
+#'     \item rating2After - Rating of player2 after the game.
+#'   }
+#'
+#'   \code{elo} always returns a matrix with two columns containing ratings
+#'   after the game. Rows represent games, columns - players.
+#'
+#' @references \href{https://en.wikipedia.org/wiki/Elo_rating_system}{Wikipedia
+#'   page} for Elo rating system.
+#'
+#' @examples
+#' # Elo ratings
+#' rate_elo(ncaa2005)
+#' add_elo_ratings(ncaa2005, initial_ratings = 100)
+#'
+#' # Elo function
+#' elo((0:12)*100, 1, 0, 0)
+#' elo((0:12)*100, 1, 0, 0, K = 10)
+#' elo((0:12)*10, 1, 0, 0, ksi = 40)
+#'
+#' @name rate-elo
+NULL
+
+#' @rdname rate-elo
+#' @export
+rate_elo <- function(cr_data, K = 30, ksi = 400,
+                     players = NULL, initial_ratings = 0) {
+  rate_iterative(
+    cr_data = cr_data,
+    rate_fun = elo_fgen(K = K, ksi = ksi),
+    players = players,
+    initial_ratings = initial_ratings
+  )
+}
+
+#' @rdname rate-elo
+#' @export
+add_elo_ratings <- function(cr_data, K = 30, ksi = 400,
+                            players = NULL, initial_ratings = 0) {
+  add_iterative_ratings(
+    cr_data = cr_data,
+    rate_fun = elo_fgen(K = K, ksi = ksi),
+    players = players,
+    initial_ratings = initial_ratings
+  )
+}
+
+#' @rdname rate-elo
+#' @export
+elo <- function(rating1, score1, rating2, score2, K = 30, ksi = 400) {
+  prob_win1 <- 1 / (1 + 10^((rating2 - rating1) / ksi))
+  game_res1 <- as.integer(score1 > score2) +
+    0.5 * dplyr::near(score1, score2)
+  rating_delta <- K * (game_res1 - prob_win1)
+
+  cbind(rating1 + rating_delta, rating2 - rating_delta)
+}
+
+elo_fgen <- function(K = 30, ksi = 400) {
+  function(rating1, score1, rating2, score2) {
+    elo_rate_fun(
+      rating1 = rating1, score1 = score1,
+      rating2 = rating2, score2 = score2,
+      K = K, ksi = ksi
+    )
+  }
+}
+
+elo_rate_fun <- function(rating1, score1, rating2, score2,
+                         K = 30, ksi = 400) {
+  prob_win1 <- 1 / (1 + 10^((rating2 - rating1) / ksi))
+  game_res1 <- as.integer(score1 > score2) +
+    0.5 * dplyr::near(score1, score2)
+  rating_delta <- K * (game_res1 - prob_win1)
+
+  c(rating1, rating2) + rating_delta * c(1, -1)
+}
---
+++ tests/testthat/test-rate-elo.R
@@ -0,0 +1,118 @@
+context("rate-elo")
+
+
+# rate_elo ----------------------------------------------------------------
+test_that("rate_elo works", {
+  output_ref <- c(-56.2377413877926, 57.9315106719875, -1.25948933788439,
+                  -29.2442777446668, 28.8099977983563)
+  names(output_ref) <- c("Duke", "Miami", "UNC", "UVA", "VT")
+
+  expect_equal(rate_elo(ncaa2005, K = 30, ksi = 400,
+                        players = NULL, initial_ratings = 0),
+               output_ref)
+})
+
+
+# add_elo_ratings ---------------------------------------------------------
+test_that("add_elo_ratings works", {
+  output_ref <- to_widecr(ncaa2005)
+  output_ref$rating1Before <-
+    c(0, -15, -29.3528000084656, -43.0885442384277,
+      15, 29.9720581625813, 44.2715899777618,
+      -0.619258154115673, 14.3831367028335, -15.5661824421677)
+  output_ref$rating2Before <-
+    c(0, 0, 0, 0,
+      14.3528000084656, 13.735744229962, 13.1491971493649,
+      -0.563787585218492, -0.510723544860779, 15.1319024958571)
+  output_ref$rating1After <-
+    c(-15, -29.3528000084656, -43.0885442384277, -56.2377413877926,
+      29.9720581625813, 44.2715899777618, 57.9315106719875,
+      14.3831367028335, -1.25948933788439, -29.2442777446668)
+  output_ref$rating2After <-
+    c(15, 14.3528000084656, 13.735744229962, 13.1491971493649,
+      -0.619258154115673, -0.563787585218492, -0.510723544860779,
+      -15.5661824421677, 15.1319024958571, 28.8099977983563)
+
+  output <- add_elo_ratings(ncaa2005, K = 30, ksi = 400,
+                            players = NULL, initial_ratings = 0)
+
+  expect_is(output, "widecr")
+  expect_identical(output[, 1:5], output_ref[, 1:5])
+  expect_equal(output$rating1Before, output_ref$rating1Before)
+  expect_equal(output$rating2Before, output_ref$rating2Before)
+  expect_equal(output$rating1After, output_ref$rating1After)
+  expect_equal(output$rating2After, output_ref$rating2After)
+})
+
+
+# elo ---------------------------------------------------------------------
+test_that("elo works", {
+  elo_rate_fun_vect <-
+    Vectorize(
+      elo_rate_fun,
+      c("rating1", "score1", "rating2", "score2", "K", "ksi")
+    )
+
+  expect_equal(elo(1:3, 1, 0, 2, K = 1, ksi = 1),
+               t(elo_rate_fun_vect(1:3, 1, 0, 2, K = 1, ksi = 1)))
+  expect_equal(elo(1:3, 1, 0, 2, K = 1:3, ksi = 1),
+               t(elo_rate_fun_vect(1:3, 1, 0, 2, K = 1:3, ksi = 1)))
+
+  expect_equal(
+    suppressWarnings(elo(1:3, 1:2, 0, -2, K = 30, ksi = 200)),
+    suppressWarnings(t(elo_rate_fun_vect(1:3, 1:2, 0, -2, K = 30, ksi = 200)))
+  )
+  expect_warning(elo(1:3, 1:2, 0, 2))
+})
+
+test_that("elo returns a matrix", {
+  expect_elo_str <- function(...) {
+    expect_true(is.matrix(...))
+    expect_true(ncol(...) == 2)
+  }
+  expect_elo_str(elo(1, 1, 0, 0))
+  expect_elo_str(elo(1:2, 1, 0, 0))
+  expect_elo_str(elo(1:2, 1:0, 0:1, 0:1))
+  expect_elo_str(elo(1:2, 1:0, 0:1, 0:1, K = c(25, 35), ksi = c(300, 400)))
+})
+
+
+# elo_fgen ----------------------------------------------------------------
+test_that("elo_fgen works", {
+  elo_generated <- elo_fgen(K = 10, ksi = 100)
+  expect_equal(
+    sapply((0:12)*10, elo_generated,
+           score1 = 1, rating2 = 0, score2 = 0),
+    sapply((0:12)*10, elo_rate_fun,
+           score1 = 1, rating2 = 0, score2 = 0,
+           K = 10, ksi = 100)
+  )
+})
+
+
+# elo_rate_fun ------------------------------------------------------------
+test_that("elo_rate_fun works", {
+  output <-
+    t(sapply((0:12)*100, elo_rate_fun,
+             score1 = 1, rating2 = 0, score2 = 0, K = 30, ksi = 400))
+  output_ref <-
+    matrix(
+      c(              15,                 -15,
+        110.798050005913,   -10.7980500059134,
+        207.207592200561,   -7.20759220056126,
+         304.52938671634,    -4.5293867163397,
+        402.727272727273,   -2.72727272727273,
+        501.597206456061,   -1.59720645606067,
+        600.919602900951,  -0.919602900951468,
+        700.524162744845,  -0.524162744845003,
+         800.29702970297,  -0.297029702970297,
+        900.167759019265,  -0.167759019265045,
+         1000.0945692755, -0.0945692754978045,
+        1100.05325368237, -0.0532536823742946,
+        1200.02997002997, -0.0299700299700256
+      ),
+      ncol = 2, byrow = TRUE
+    )
+
+  expect_equal(output, output_ref)
+})
